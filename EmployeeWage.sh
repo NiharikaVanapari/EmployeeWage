c281a6051e24be486fa5b6a32a782c69fdd901a4
NiharikaVanapari <niharikavanapari23@gmail.com>
2021-06-07 19:58:26 +0530

    Employee Check

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,3 +1,11 @@
 echo "Welcome to Employee Wage Computation"
-
+isPresent=0
+isAbsent=1
+empcheck=$((RANDOM%2))
+if [ $isPresent -eq $empcheck ]
+then 
+     echo "Employee is Present"
+else
+     echo "Employee is Absent"
+fi
 
