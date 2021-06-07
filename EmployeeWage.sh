ae8fa8eb3a226cd84df18bce7a75360b62d990cb
NiharikaVanapari <niharikavanapari23@gmail.com>
2021-06-07 20:19:02 +0530

    SwitchCaseStatement

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -4,19 +4,19 @@ Parttime=1
 isAbsent=2
 empwageperhr=20
 empcheck=$((RANDOM%3))
-if [ $Fulltime -eq $empcheck ]
-then 
-    workinghr=8
-     echo "Employee is Fulltime"
-elif [ $Parttime -eq $empcheck ]
-then
-    workinghr=4
-     echo "Employee is Parttime"
-
-else
-    workinghr=0
-     echo "Employee is Absent"
-
-fi
+case $empcheck in
+       $Fulltime)
+       workinghr=8
+       echo "Employee is Fulltime"
+       ;;
+       $Parttime)
+       workinghr=4
+       echo "Employee is Parttime"
+       ;;
+       $isAbsent)
+       workinghr=0
+       echo "Employee is Absent"
+       ;;
+esac
  employeewage=$((empwageperhr*workinghr))
 echo "Employee Wage: " $employeewage
