6b9c1647d7cc8a722a82d10e79f1438eca362df9
NiharikaVanapari <niharikavanapari23@gmail.com>
2021-06-07 20:02:52 +0530

    Wage

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,11 +1,16 @@
 echo "Welcome to Employee Wage Computation"
 isPresent=0
 isAbsent=1
+empwageperhr=20
 empcheck=$((RANDOM%2))
 if [ $isPresent -eq $empcheck ]
 then 
+    workinghr=8
      echo "Employee is Present"
 else
+    workinghr=0
      echo "Employee is Absent"
-fi
 
+fi
+ employeewage=$((empwageperhr*workinghr))
+echo "Employee Wage: " $employeewage
