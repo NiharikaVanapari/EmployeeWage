31b52fb9e589796ec0d91b6ab699852f95c033cc
NiharikaVanapari <niharikavanapari23@gmail.com>
2021-06-17 18:11:20 +0530

    MonthlyWage

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,13 +1,14 @@
 echo "Welcome to Employee Wage Computation"
-declare -a employeedailywage
 Fulltime=0
 Parttime=1
 isAbsent=2
-monthsalary=0
 empwageperhr=20
-counter=0
-#assume 20 working days in a month
-for (( i=0; i<20; i++ ))
+workingdays=20
+maxHrsinMonth=100
+monthsalary=0
+TotalWorkingHr=0
+
+for (( i=0; i<20 && $TotalWorkingHr<100; i++ ))
 do
 	empcheck=$((RANDOM%3))
 	case $empcheck in
@@ -21,12 +22,7 @@ do
        		workinghr=0
        		;;
 	esac
- 	employeedailywage[((counter++))]=$((empwageperhr*workinghr))
+ 	TotalWorkingHr=$((TotalWorkingHr+workinghr))
 done
-echo "Employee Daily Wage : ${employeedailywage[@]}"
-for (( i=0; i<20; i++ ))
-do
-	sal=${employeedailywage[i]}
-	monthsalary=$((monthsalary+sal))
-done
+monthsalary=$((empwageperhr*TotalWorkingHr))
 echo "Employee Monthly Wage:  $monthsalary"
