5bc764a4147353fb0348922e42fe9129146830b7
NiharikaVanapari <niharikavanapari23@gmail.com>
2021-06-17 18:15:38 +0530

    RefactoringUsingFunctions

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -8,9 +8,7 @@ maxHrsinMonth=100
 monthsalary=0
 TotalWorkingHr=0
 
-for (( i=0; i<20 && $TotalWorkingHr<100; i++ ))
-do
-	empcheck=$((RANDOM%3))
+function getWorkingHrs() {
 	case $empcheck in
        	     $Fulltime)
        		workinghr=8
@@ -22,7 +20,15 @@ do
        		workinghr=0
        		;;
 	esac
- 	TotalWorkingHr=$((TotalWorkingHr+workinghr))
+
+ 	echo $workinghr
+}
+for (( i=0; i<20 && $TotalWorkingHr<100; i++ ))
+do
+ 	empcheck=$((RANDOM%3))
+	workinghr="$( getWorkingHrs $empcheck )"
+	TotalWorkingHr=$((TotalWorkingHr+workinghr))
+
 done
 monthsalary=$((empwageperhr*TotalWorkingHr))
 echo "Employee Monthly Wage:  $monthsalary"
