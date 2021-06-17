b8f51675fa26cb0bd88fc19202e7956e39a73630
NiharikaVanapari <niharikavanapari23@gmail.com>
2021-06-17 18:05:53 +0530

    SwitchCase

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,22 +1,32 @@
 echo "Welcome to Employee Wage Computation"
+declare -a employeedailywage
 Fulltime=0
 Parttime=1
 isAbsent=2
+monthsalary=0
 empwageperhr=20
-empcheck=$((RANDOM%3))
-case $empcheck in
-       $Fulltime)
-       workinghr=8
-       echo "Employee is Fulltime"
-       ;;
-       $Parttime)
-       workinghr=4
-       echo "Employee is Parttime"
-       ;;
-       $isAbsent)
-       workinghr=0
-       echo "Employee is Absent"
-       ;;
-esac
- employeewage=$((empwageperhr*workinghr))
-echo "Employee Wage: " $employeewage
+counter=0
+#assume 20 working days in a month
+for (( i=0; i<20; i++ ))
+do
+	empcheck=$((RANDOM%3))
+	case $empcheck in
+       	     $Fulltime)
+       		workinghr=8
+     		;;
+             $Parttime)
+       		workinghr=4
+       		;;
+             $isAbsent)
+       		workinghr=0
+       		;;
+	esac
+ 	employeedailywage[((counter++))]=$((empwageperhr*workinghr))
+done
+echo "Employee Daily Wage : ${employeedailywage[@]}"
+for (( i=0; i<20; i++ ))
+do
+	sal=${employeedailywage[i]}
+	monthsalary=$((monthsalary+sal))
+done
+echo "Employee Monthly Wage:  $monthsalary"
